8f9a1f6bc0b37a7faf8cd5e3c222a827b2d3ee1b
Allen Hulley <hulleyaj@gmail.com>
2020-10-20 21:21:25 -0400

    top half good, bottom half bad

--- App.js
+++ App.js
@@ -1,57 +1,37 @@
-import React, { useState, useEffect } from 'react';
-import { Platform, Text, View, StyleSheet } from 'react-native';
-import Constants from 'expo-constants';
-import * as Location from 'expo-location';
+import { StatusBar } from 'expo-status-bar';
+import React from 'react';
+import { SafeAreaProvider } from 'react-native-safe-area-context';
+import { Provider } from 'react-redux';
+import { createStore, combineReducers } from 'redux';
+import { counterReducer } from 'll-pkg';
+
+import useCachedResources from './hooks/useCachedResources';
+import useColorScheme from './hooks/useColorScheme';
+import Navigation from './navigation';
 import NativeScreen from './screens/NativeScreen';
 
+const store = createStore(
+  combineReducers({
+    counterReducer
+  })
+);
+
+
 export default function App() {
-  const [location, setLocation] = useState(null);
-  const [errorMsg, setErrorMsg] = useState(null);
+  const isLoadingComplete = useCachedResources();
+  const colorScheme = useColorScheme();
 
-  useEffect(() => {
-    if (Platform.OS === 'android' && !Constants.isDevice) {
-      setErrorMsg(
-        'Oops, this will not work on Sketch in an Android emulator. Try it on your device!'
-      );
-    } else {
-      (async () => {
-        let { status } = await Location.requestPermissionsAsync();
-        if (status !== 'granted') {
-          setErrorMsg('Permission to access location was denied');
-        }
-
-        let location = await Location.getCurrentPositionAsync({});
-        setLocation(location);
-      })();
-    }
-  });
-
-  let text = 'Waiting1..';
-  if (errorMsg) {
-    text = errorMsg;
-  } else if (location) {
-    text = JSON.stringify(location);
+  if (!isLoadingComplete) {
+    return null;
+  } else {
+    return (
+      <SafeAreaProvider>
+        <Provider store={store}>
+          <NativeScreen />
+          <Navigation colorScheme={colorScheme} />
+          <StatusBar />
+        </Provider>
+      </SafeAreaProvider>
+    );
   }
-
-  return (
-    <View style={styles.container}>
-      <Text style={styles.paragraph}>{text}</Text>
-      <NativeScreen />
-    </View>
-  );
 }
-
-const styles = StyleSheet.create({
-  container: {
-    flex: 1,
-    alignItems: 'center',
-    justifyContent: 'center',
-    paddingTop: Constants.statusBarHeight,
-    backgroundColor: '#ecf0f1',
-  },
-  paragraph: {
-    margin: 24,
-    fontSize: 18,
-    textAlign: 'center',
-  },
-});
