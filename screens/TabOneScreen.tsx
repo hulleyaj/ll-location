872d27160d0a67463dbd625693e88edcd96399e1
Allen Hulley <hulleyaj@gmail.com>
2020-10-20 22:05:32 -0400

    Buttons from ll-pkg break it

--- screens/TabOneScreen.tsx
+++ screens/TabOneScreen.tsx
@@ -1,38 +1,32 @@
 import * as React from 'react';
-import { StyleSheet } from 'react-native';
+import { StyleSheet, Button } from 'react-native';
 import { connect } from 'react-redux';
-import { Button, decrementAction, incrementAction } from 'll-pkg';
+import { decrementAction, incrementAction } from 'll-pkg';
 import { Text, View } from '../components/Themed';
 
-// function TabOneScreen({ decrementAction, count, incrementAction}) {
-//   return (
-//     <View style={styles.container}>
-//       <Button text="-" onClick={decrementAction} />
-//       <Text>{count}</Text>
-//       <Button text="+" onClick={incrementAction} />
-//     </View>
-//   );
-// }
-
-// const mapStateToProps = (state) => ({
-//   count: state.counterReducer.count
-// })
-
-// const mapDispatchToProps = {
-//   decrementAction,
-//   incrementAction
-// }
-
-// export default connect(
-//   mapStateToProps,
-//   mapDispatchToProps
-// )(TabOneScreen);
-
-export default function TabOneScreen() {
-  return <View>
-    <Text>TABONE</Text>
-  </View>
+function TabOneScreen({ decrementAction, count, incrementAction}) {
+  return (
+    <View style={styles.container}>
+      <Button title="-" onPress={decrementAction} />
+      <Text>{count}</Text>
+      <Button title="+" onPress={incrementAction} />
+    </View>
+  );
 }
+
+const mapStateToProps = (state) => ({
+  count: state.counterReducer.count
+})
+
+const mapDispatchToProps = {
+  decrementAction,
+  incrementAction
+}
+
+export default connect(
+  mapStateToProps,
+  mapDispatchToProps
+)(TabOneScreen);
 
 const styles = StyleSheet.create({
   container: {
